53bcd2a2769a5cd0c59682410cba353dc38fc113
glaz0ff <glazzer0@mail.ru>
2023-10-24 01:12:22 +0500

    add jwt auth

--- config/auth.php
+++ config/auth.php
@@ -14,10 +14,9 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'api',
         'passwords' => 'users',
     ],
-
     /*
     |--------------------------------------------------------------------------
     | Authentication Guards
@@ -38,6 +37,10 @@ return [
     'guards' => [
         'web' => [
             'driver' => 'session',
+            'provider' => 'users',
+        ],
+        'api' => [
+            'driver' => 'jwt',
             'provider' => 'users',
         ],
     ],
