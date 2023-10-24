a59cce645b325efb9d1495532ad95869eada906b
d.shvetsov <inspairax230801@mail.ru>
2023-10-24 19:19:00 +0500

    edit model for mongo

--- app/Models/EntityValue.php
+++ app/Models/EntityValue.php
@@ -6,12 +6,8 @@ use MongoDB\Laravel\Eloquent\Model;
 
 class EntityValue extends Model
 {
-    use HasFactory;
+    protected $connection = 'mongodb';
 
     protected $table = 'entity_values';
     protected $guarded = false;
-
-    public function entityField() {
-        return $this->belongsTo(EntityField::class, 'instance_id', 'id');
-    }
 }
