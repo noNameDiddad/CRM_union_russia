4aa1ad0132c6b00fd35c7bb013a52191b050c4b9
d.shvetsov <inspairax230801@mail.ru>
2023-12-05 14:50:33 +0500

    Удаляет лишнее

--- app/Services/FieldService.php
+++ app/Services/FieldService.php
@@ -4,23 +4,4 @@ namespace App\Services;
 
 class FieldService
 {
-    public function index()
-    {
-
-    }
-
-    public function create()
-    {
-
-    }
-
-    public function update()
-    {
-
-    }
-
-    public function delete()
-    {
-
-    }
 }
