7abd9874419ab3fab4c76096d365306bb0eebc50
d.shvetsov <inspairax230801@mail.ru>
2023-12-09 21:59:53 +0500

    добавляет логику передачи фильтрованных значений и правит постман коллекцию

--- app/Services/EntityService.php
+++ app/Services/EntityService.php
@@ -19,6 +19,16 @@ class EntityService
         $this->repository = app(EntityRepository::class);
     }
 
+    public function getAll()
+    {
+        return $this->repository->all();
+    }
+
+    public function getFiltered()
+    {
+        return $this->repository->where('is_sub_entity', false)->get();
+    }
+
     public function create(array $data): Model
     {
         $data['hash'] = Str::slug($data['name']);
