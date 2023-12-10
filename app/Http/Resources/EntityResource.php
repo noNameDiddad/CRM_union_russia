ef44d887cfbdb0595b5a4f98b1eefa08a80db86b
d.shvetsov <inspairax230801@mail.ru>
2023-12-10 16:41:40 +0500

    добавляет список подсущностей

--- app/Http/Resources/EntityResource.php
+++ app/Http/Resources/EntityResource.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Resources;
 
+use App\Helpers\EntityFieldHelper;
+use App\Repositories\EntityFieldRepository;
+use App\Repositories\EntityRepository;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,10 +17,22 @@ class EntityResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $fields = app(EntityFieldHelper::class)->getFields($this->id);
+        $fields = collect($fields)->whereNotNull("relateTo");
+        if ($fields->count() != 0) {
+            $subEntities = [];
+            foreach ($fields as $field) {
+                $subEntities[$field['relateTo']] = app(EntityRepository::class)->where('hash', $field['relateTo'])->first()->id;
+            }
+        }
+
+
+
         return [
             'id' => $this->id,
             'name' => $this->name,
             'hash' => $this->hash,
+            'subEntities' => $subEntities ?? [],
             'is_kanban' => $this->is_kanban,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
