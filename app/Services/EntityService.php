3a56201dd521c121a461627eec116e5e2d622fcc
d.shvetsov <inspairax230801@mail.ru>
2023-12-10 16:24:40 +0500

    рефактр кода

--- app/Services/EntityService.php
+++ app/Services/EntityService.php
@@ -19,11 +19,6 @@ class EntityService
         $this->repository = app(EntityRepository::class);
     }
 
-    public function getAll()
-    {
-        return $this->repository->all();
-    }
-
     public function getFiltered()
     {
         return $this->repository->where('is_sub_entity', false)->get();
